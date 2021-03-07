561a69c938d8a5ac1f1628709f3b4b039fcdc288
men01 <men01@DESKTOP-VD9RB20>
2021-03-08 02:11:44 +0900

    add getWkday

--- DalApp/src/dalstudy/DalApp.java
+++ DalApp/src/dalstudy/DalApp.java
@@ -8,11 +8,24 @@ public class DalApp {
 		else if(oyear%400==0) Yun=2;
 		return Yun;
 	}
-
+	
+	 int getWkday(int oyear) {
+		int wday=0, yuncount=0, totdays=0;
+		for(int y=1; y<oyear; y++) {
+			if(isYun(y)!=0) ++yuncount;
+		}
+		totdays=(oyear-1)*365 + yuncount;
+		wday = totdays%7; // 12월 31일의 요일
+		wday = ++wday%7; // 1월 1일의 요일
+		return wday;
+	}
+	
+	 
 	public static void main(String[] args) {
 		int wd=0, oyear=2012;
 		DalApp dal = new DalApp();
 		System.out.println("Yun = " + dal.isYun(oyear));
+		
 	}
-
+	
 }
